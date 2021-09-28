0c8517ddc999e7cc5f28a4f2303e1c569b475bd7
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-09-28 11:05:00 +0530

    Add:find maximum value of float using junit test case

--- src/test/java/com/bridgelabz/GenricsDay13/testMaximum.java
+++ src/test/java/com/bridgelabz/GenricsDay13/testMaximum.java
@@ -4,15 +4,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class testMaximum{
-		@Test
-		public void testInteger(){
-			
-			Maximum object=new Maximum();
-			int actual=object.getmaximum(499, 200, 340);
-			Assert.assertEquals(499,actual ,0);
-		}
+	@Test
+	public void testInteger(){
+		int actual=Maximum.getmaximum(499, 200, 340);
+		Assert.assertEquals(499,actual ,0);
+	}
 
-}
-/**
+
+	/**
 		in this case the junit is sucess values is matched 
-	*/
+	 */
+	//floating test case
+	@Test
+	public void float1() {
+		float actualValue = Maximum.getmaximum1(12f,14f,15f);
+		Assert.assertEquals(15,actualValue ,0);
+	}
+
+	@Test
+	public void float2() {
+		float actualValue = Maximum.getmaximum1(1000f,545f,746f);
+		Assert.assertEquals(1000,actualValue, 0);
+	}
+}
