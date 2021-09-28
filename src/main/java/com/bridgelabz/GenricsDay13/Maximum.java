81645cfdacf73545e5f25716485961543bc37324
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-09-28 11:38:08 +0530

    Add:mergeing all three method into one using genrics

--- src/main/java/com/bridgelabz/GenricsDay13/Maximum.java
+++ src/main/java/com/bridgelabz/GenricsDay13/Maximum.java
@@ -2,7 +2,9 @@ package com.bridgelabz.GenricsDay13;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Maximum {
+public class Maximum <E extends Comparable> {
+	E value1, value2, value3;
+
 	private  final static Logger Log= LogManager.getLogger(Maximum.class);
 
 	public static Integer getmaximum(Integer value1, Integer value2, Integer value3) 
@@ -38,6 +40,28 @@ public class Maximum {
 		Log.info(max);
 		return max;
 	}
+	public Maximum(E value1, E value2, E value3) {
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
 
+	public E testMaximum() {
+		E testMax = testMaximum(value1, value2, value3);
+		return testMax;
+	}
+	public static <E extends Comparable> E testMaximum(E value1, E value2, E value3) {
+		E max = value1;
+		if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+			max = value2;
+		else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+			max = value3;
+		toPrint(value1, value2, value3, max);
+		return max;
+	}
+
+	private static <E> void toPrint(E value1, E value2, E value3, E max) {
+		System.out.println("Max of "+value1+" , "+value2+" , "+value3+" is: "+max);
+	}
 }
 
