52a169c00958f516d299869d03c2098da735d15c
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-09-28 11:00:45 +0530

    Add:maximum value of float using junit

--- src/main/java/com/bridgelabz/GenricsDay13/Maximum.java
+++ src/main/java/com/bridgelabz/GenricsDay13/Maximum.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Logger;
 public class Maximum {
 	private  final static Logger Log= LogManager.getLogger(Maximum.class);
 
-	public Integer getmaximum(Integer value1, Integer value2, Integer value3) 
+	public static Integer getmaximum(Integer value1, Integer value2, Integer value3) 
 	{
 		Integer max = value1;
 		if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
@@ -17,5 +17,17 @@ public class Maximum {
 
 		return max;
 	}
-}
+	//to find maximum float value
+	public static Float getmaximum1(Float value1, Float value2, Float value3) 
 
+	{
+		Float max = value1;
+		if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+			max = value2;
+		else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+			max = value3;
+		Log.info(max);
+		return max;
+	}
+
+}
